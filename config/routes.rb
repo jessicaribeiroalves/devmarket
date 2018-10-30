5c660f0fe91b74bb37cd1a7b7de57509aa209924
Theodore Chuang <chuangtheodore@gmail.com>
2018-10-30 22:29:25 +1100

    Set universal css class colors, fonts. Update Navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users, path: "", path_names: {sign_in: "login", sign_out: "logout", sign_up: "register"}
-  
+
   root "home#index"
 
-
-  get 'products', to: 'products#index'
-  post 'projects', to: 'projects#create'
-  get 'projects/new', to: 'projects#new', as: 'new_project'
-
-  get "users/sign_in"
+  get "projects/new", to: "projects#new", as: "new_project"
   get "projects", to: "projects#index"
-       
-  
+  post "projects", to: "projects#create"
+
+  get "products", to: "products#index"
+
   # get 'users/show'
   # post 'users/create'
   # get ‘projects/new’
@@ -19,8 +16,4 @@ Rails.application.routes.draw do
   # delete 'users/destroy'
   # put 'users/update'
   # patch 'users/update'
-
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
 end
