e1904650736e253b5b0b9106ef2485f02e6ac071
Theodore Chuang <chuangtheodore@gmail.com>
2018-10-30 13:41:57 +1100

    User seed data, configured routes, basic links in application view

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+1.times do |i|
+  User.create(
+    user_type: "client",
+    email: "clientpart#{i}@mail.com",
+    password: "password",
+  )
+  User.create(
+    user_type: "client",
+    email: "clientfull#{i}@mail.com",
+    password: "password",
+    name: "Client Full Profile",
+    company_name: "Need Help Inc.",
+    phone_number: 212345678,
+    bio: "need lots of help, find me a dev",
+    city: "Sydney",
+  )
+end
+
+1.times do |i|
+  User.create(
+    user_type: "dev",
+    email: "devpart#{i}@mail.com",
+    password: "password",
+  )
+  User.create(
+    user_type: "dev",
+    email: "devfull#{i}@mail.com",
+    password: "password",
+    name: "Dev Full Profile",
+    company_name: "Dev for You Inc.",
+    phone_number: 287654321,
+    bio: "ninja dev, skilled in all tech",
+    city: "Sydney",
+  )
+end
