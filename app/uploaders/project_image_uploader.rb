1fecda1e0c7453901324f5310e8e8bf7fcd8d722
Theodore Chuang <chuangtheodore@gmail.com>
2018-10-31 22:31:33 +1100

    Add carrierwave, carrierwave-aws. Uploader for User avatar

---
+++ app/uploaders/project_image_uploader.rb
@@ -0,0 +1,14 @@
+class ProjectImageUploader < CarrierWave::Uploader::Base
+  # Choose what kind of storage to use for this uploader:
+  storage :aws
+
+  # Override the directory where uploaded files will be stored.
+  # This is a sensible default for uploaders that are meant to be mounted:
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  def extension_whitelist
+    %w(jpg jpeg gif png)
+  end
+end
