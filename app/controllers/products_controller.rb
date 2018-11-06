5db26aae4ae514995342424c0fc79d77475ea61c
natashamunasinghe <natashakhema@hotmail.com>
2018-11-06 11:56:03 +1100

    connect product project pages

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,10 +1,9 @@
 class ProductsController < ApplicationController
   def index
     @products = Product.all
-    @product_1 = Product.find_by(option: "Basic Website")
-    @product_2 = Product.find_by(option: "Ecommerce Website")
-    @product_3 = Product.find_by(option: "Blog Site")
+    @product_1 = Product.find(1)
+    @product_2 = Product.find(2)
+    @product_3 = Product.find(3)
   end
 
-  
 end
